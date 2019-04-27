80af8c4396ac04a61ace38dfbeb3a28ed9516252
Alana Domit Bittar <alanadomitbittar@protonmail.com>
2019-04-27 04:52:36 +0000

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -2,6 +2,7 @@ from random import randint
 from bisect import bisect_left
 from string import ascii_letters
 
+
 def generate_alphabet_dict():
     alphabet_list = list(ascii_letters)
     alphabet_values = list(range(1, 53))
@@ -20,7 +21,7 @@ def generate_unordered_list(range_value):
 def is_prime(number):
     if number > 1:
         for i in range(2, number):
-            if (number % i) == 0:
+            if (number % i) is 0:
                 return False
         else:
             return True
@@ -31,7 +32,7 @@ def number_search(ordered_list, number):
     low = 0
     high = len(ordered_list) - 1
     position = bisect_left(ordered_list, number, low, high)
-    if(ordered_list[position] == number):
+    if(ordered_list[position] is number):
         print("The number is at position {}".format(position))
     else:
         print("The number isn't in the list")
@@ -41,7 +42,7 @@ def compare_lists():
     bigger_list = generate_unordered_list(5000)
     smaller_list_set = set(smaller_list)
     intersection = smaller_list_set.intersection(bigger_list)
-    if bool(intersection) == False:
+    if bool(intersection) is False:
         print("There's nothing in common between the lists")
     else:
         print("Common elements: {}".format(intersection))
@@ -49,7 +50,7 @@ def compare_lists():
 def find_prime_numbers(number):
     for n1 in range(2, number + 1):
         for n2 in range(2, n1):
-            if n1 % n2 == 0:
+            if n1 % n2 is 0:
                 break
         else:
             print(n1)
@@ -70,7 +71,7 @@ list_ = generate_ordered_list(300)
 alphabet_dict = generate_alphabet_dict()
 
 answer = None
-while answer != "5":
+while answer is not "5":
     print("""
     1. Find out if a number is in a random 300 number ordered list
     2. Compare two random unordered number lists
@@ -79,24 +80,24 @@ while answer != "5":
     5. Quit
     """)
     answer = input("Please, choose an option: ")
-    if answer == "1":
+    if answer is "1":
         try:
             number = int(input("Choose a number: "))
             number_search(list_, number)
         except ValueError:
             print("That's not a valid number")
-    elif answer == "2":
+    elif answer is "2":
         compare_lists()
-    elif answer == "3":
+    elif answer is "3":
         try:
             number = int(input("Choose a number: "))
             find_prime_numbers(number)
         except ValueError:
             print("That's not a valid number.")
-    elif answer == "4":
+    elif answer is "4":
         word = input("Choose a word: ")
         is_word_prime(word, alphabet_dict)
-    elif answer == "5":
+    elif answer is "5":
         print("\n Bye!") 
     else:
        print("\n Not a valid option. Try again.")
